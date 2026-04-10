38789b26697d65e9f0f06b176abe1f22a16ed8ce
ajayvenkat2003 <tvdajay2003@gmail.com>
2026-04-10 12:07:29 +0530

    dvc commit1

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,8 @@
-
-def add(a,b):
-    return a+b
-
-def sub(a,b):
-    return a-b
+import sys,os
+import pandas as pd
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import torch
+from torch import nn
+t = torch.randint(1,2,(2,2))
+t.type_as(torch.float16)
+t._grad
